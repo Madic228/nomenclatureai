ba71cecb57d406086161b9a0b03879fed36b5a36
Rzayev Metin <rzayevmetin@mail.ru>
2024-06-28 14:06:11 +0500

    корректировка принтов и логов. повтор генерации токена

--- ImageGeneration/main.py
+++ ImageGeneration/main.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from decouple import config
 import base64
+import time
 
 class TokenManager:
     def __init__(self, auth_token, scope='GIGACHAT_API_PERS'):
@@ -12,12 +13,6 @@ class TokenManager:
         self.giga_token = None
 
     def get_token(self):
-        """
-        Выполняет POST-запрос к эндпоинту, который выдает токен.
-
-        Возвращает:
-        - токен (str): Токен авторизации.
-        """
         rq_uid = str(uuid.uuid4())
         url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
         headers = {
@@ -32,10 +27,9 @@ class TokenManager:
             response.raise_for_status()
             self.giga_token = response.json().get('access_token')
         except requests.RequestException as e:
-            print(f"Ошибка: {str(e)}")
-            return None
+            print(f"Ошибка при создании токена: {str(e)}")
+            self.giga_token = None
         return self.giga_token
-
 
 class GigaChatClient:
     def __init__(self, token_manager):
@@ -43,15 +37,6 @@ class GigaChatClient:
         self.giga_token = token_manager.giga_token
 
     def send_chat_request(self, user_message):
-        """
-        Отправляет POST-запрос к API GigaChat для получения ответа от модели чата.
-
-        Параметры:
-        - user_message (str): Сообщение пользователя, которое будет обработано моделью GigaChat.
-
-        Возвращает:
-        - str: Строка сгенерированного ответа GigaChat с тэгом img.
-        """
         url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
         headers = {
             'Content-Type': 'application/json',
@@ -83,16 +68,6 @@ class GigaChatClient:
             return None
 
     def download_image(self, img_tag, output_filename):
-        """
-        Скачивает изображение по URL из тега img, сохраняет его в файл и конвертирует в base64.
-
-        Параметры:
-        - img_tag (str): HTML-тег img, содержащий URL изображения.
-        - output_filename (str): Имя файла для сохранения изображения.
-
-        Возвращает:
-        - str: Изображение в формате base64.
-        """
         soup = BeautifulSoup(img_tag, 'html.parser')
         img_src = soup.img['src']
         url = f'https://gigachat.devices.sberbank.ru/api/v1/files/{img_src}/content'
@@ -103,25 +78,26 @@ class GigaChatClient:
         try:
             response = requests.get(url, headers=headers, verify=False)
             response.raise_for_status()
-            with open(output_filename, 'wb') as f:
-                f.write(response.content)
-            print(f"Image saved as {output_filename}")
-
-            # Конвертируем изображение в base64
-            with open(output_filename, "rb") as image_file:
-                encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+            encoded_string = base64.b64encode(response.content).decode('utf-8')
             return encoded_string
         except requests.RequestException as e:
             print(f"Произошла ошибка при загрузке изображения: {str(e)}")
             return None
 
-
 class ProductImageGeneration:
     def __init__(self):
         auth = config('AUTH', default='')
         self.token_manager = TokenManager(auth)
-        self.token_manager.get_token()
+        self.obtain_token()
         self.giga_chat_client = GigaChatClient(self.token_manager) if self.token_manager.giga_token else None
+
+    def obtain_token(self):
+        for _ in range(3):  # 3 попытки получения токена при сбое
+            token = self.token_manager.get_token()
+            if token:
+                break
+            print("Не удалось получить токен, повторная попытка...")
+            time.sleep(2)  # Ожидание 2 секунды между попытками
 
     def run(self, product_name):
         if not self.giga_chat_client:
@@ -145,16 +121,14 @@ class ProductImageGeneration:
                 base64_image = self.giga_chat_client.download_image(response_img_tag, output_filename)
                 if base64_image:
                     base64_images.append((user_message, base64_image))
-                    print(f"Base64 encoded image {i + 1}:")
-                    print(base64_image)
 
         return base64_images
 
-
-# # Запуск основной функции и получение результатов
-# giga_chat_service = ProductImageGeneration()
-# base64_images = giga_chat_service.run()
-# if base64_images:
-#     for user_message, base64_image in base64_images:
-#         print(f"Request: {user_message}")
-#         print(f"Base64: {base64_image}")
+# Запуск основной функции и получение результатов
+giga_chat_service = ProductImageGeneration()
+product_name = "деревенское сливочное масло"
+base64_images = giga_chat_service.run(product_name)
+if base64_images:
+    for user_message, base64_image in base64_images:
+        print(f"Запрос: {user_message} \n Изображение закодировано.")
+        # print(f"Кодировка: {base64_image}")
